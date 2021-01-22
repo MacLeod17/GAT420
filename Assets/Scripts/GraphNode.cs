1cd50e71334594b7fdaec99461dd19fe5a0d850d
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-01-22 10:36:30 -0700

    Graph Nodes
    Created a Search scene, GraphNode and GraphNodeCreator scripts, and GraphNode prefab.

---
+++ Assets/Scripts/GraphNode.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphNode : SearchNode
+{
+    public struct Edge
+    {
+        public GraphNode nodeA;
+        public GraphNode nodeB;
+    }
+
+    public List<Edge> Edges { get; set; } = new List<Edge>();
+
+    void Update()
+    {
+        foreach(Edge edge in Edges)
+        {
+            Debug.DrawLine(edge.nodeA.transform.position, edge.nodeB.transform.position);
+        }
+    }
+}
